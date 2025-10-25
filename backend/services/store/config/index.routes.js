79fd83ae4e5d27965d20911cf5add8e22bfd825f
TheRealJ-OnCode <therealjonweb@gmail.com>
2025-10-26 01:29:00 +0400

    /health route deleted

--- backend/services/store/config/index.routes.js
+++ backend/services/store/config/index.routes.js
@@ -1,6 +1,6 @@
 //All routes exports from this file
 const router = require("express").Router();
-router.use(require("../modules/health/health.routes"));
+// router.use(require("../modules/health/health.routes"));
 router.use(require("../modules/category/category.routes"));
 router.use(require("../modules/order/order.routes"));
 router.use(require("../modules/product/product.routes"));
